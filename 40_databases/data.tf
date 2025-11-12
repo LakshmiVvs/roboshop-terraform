0bdda63faa39b0d0c4a4634b9b8d862b15e4ab40
LakshmiVvs <vvslakshmi791@gmail.com>
2025-11-12 12:24:03 +0530

    terra

--- 40_databases/data.tf
+++ 40_databases/data.tf
@@ -15,7 +15,7 @@ data "aws_ssm_parameter" "bastion_sg_id" {
   name = "/${var.project_name}/${var.environment}/bastion_sg_id"
 }
 
-data "aws_ssm_parameter" "public_subnet_ids" {
-name  = "/${var.project_name}/${var.environment}/public_subnet_ids"
+data "aws_ssm_parameter" "database_subnet_id" {
+name  = "/${var.project_name}/${var.environment}/database_subnet_id"
 
 }
