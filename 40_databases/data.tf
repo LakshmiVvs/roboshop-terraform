55ccceed293f0e99c87b031cb036f48f88e7468f
LakshmiVvs <vvslakshmi791@gmail.com>
2025-11-12 13:37:17 +0530

    terra1

--- 40_databases/data.tf
+++ 40_databases/data.tf
@@ -1,8 +1,15 @@
 data "aws_ami" "joindevops" {
-
+    owners           = ["973714476881"]
+    most_recent      = true
+    
     filter {
         name   = "name"
         values = ["RHEL-9-DevOps-Practice"]
+    }
+
+    filter {
+        name   = "root-device-type"
+        values = ["ebs"]
     }
 
     filter {
