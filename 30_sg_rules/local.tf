3bd6cb26dcecfdb9971c3664890a1e226821407b
LakshmiVvs <vvslakshmi791@gmail.com>
2025-11-12 12:14:00 +0530

    terra

--- 30_sg_rules/local.tf
+++ 30_sg_rules/local.tf
@@ -4,4 +4,6 @@ locals {
     bastion_sg_id = data.aws_ssm_parameter.bastion_sg_id.value
 
     mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
+
+    ami_id = data.aws_ami.joindevops.id
 }
--- 30_sg_rules/main.tf
+++ 30_sg_rules/main.tf
@@ -16,3 +16,13 @@ resource "aws_security_group_rule" "bastion_laptop" {
   cidr_blocks = ["0.0.0.0/0"]
   security_group_id = local.bastion_sg_id
 }
+
+resource "aws_security_group_rule" "mongodb_bastion" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  source_security_group_id   = local.bastion_sg_id
+  security_group_id = local.mongodb_sg_id
+}
+
